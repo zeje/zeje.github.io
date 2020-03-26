4d736a737df08a75780e6bbc74c29540f1d1add1
蔡泽智 <zezhi821@hotmail.com>
2020-03-27 02:13:01 +0800

    sh脚本

--- centos_deploy.sh
+++ centos_deploy.sh
@@ -1,7 +1,25 @@
-## git clone https://github.com/zeje/zeje.github.io.git
+# 安装nodejs
+
+# yum install nodejs
+
+# 安装vuepress
+
+# npm install -g vuepress
+
+# 拉取源码
+
+# git clone https://github.com/zeje/zeje.github.io.git
+
+# 切换目录
+
+# cd /root/zeje.github.io
 
 # 拉取最新代码
 git pull origin master
 
 # 构建
 vuepress build
+
+# 重启nginx
+
+nginx -s reload
